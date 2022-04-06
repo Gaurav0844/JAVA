ffacc8a4ef3cbd9c64fbf84493b3726a29955060
Gaurav0844 <ranganekarg@gmail.com>
2022-04-06 16:50:38 +0530

    First

---
+++ JAVA/src/module-info.java
@@ -0,0 +1,10 @@
+class third {
+    int a=2; //instance variable
+    static double b=50; //static variable
+    public static void main(String[] args)
+    {
+        boolean c=false;
+        third t=new third();
+        System.out.println(t.a+" "+b+" "+c);
+    }
+}
